c457c29a87a36df9a2e1ea917d5a34e68e92fc51
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2025-01-01 15:14:50 -0300

    Changed the tests for GTest

--- test/main.cpp
+++ test/main.cpp
@@ -1,62 +1,47 @@
-#include <iostream>
-#include <stdexcept>
-#include <string>
 #include "exceptdefs.h"
 #include "utils.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
 #include <segvcatch.h>
+#include <stdexcept>
+#include <string>
 
 using namespace std;
 
-void handle_segv()
-{
-    throw std::runtime_error("My SEGV");
-}
+void handle_segv() { throw std::runtime_error("My SEGV"); }
 
-void handle_fpe()
-{
-    throw std::runtime_error("My FPE");
-}
+void handle_fpe() { throw std::runtime_error("My FPE"); }
 
-int main(int argc, char *argv[])
-{
+TEST(Segvcatch, InitSegv) {
     segvcatch::init_segv(nullptr);
     segvcatch::init_fpe(nullptr);
 
-    try
-    {
+    try {
         /*
-        * In my tests with ARM, this try-catch block is optimized away by the compiler without some other code that can throw an exception.
-        */
+         * In my tests with ARM, this try-catch block is optimized away by the
+         * compiler without some other code that can throw an exception.
+         */
         std::cout << "Do not optimize away the try-catch block!" << std::endl;
 
         segvcatch::donot_optimize_away();
         test::causes_segv();
-    }
-    catch (const segvcatch::hardware_exception &e)
-    {
-        std::cerr << "Exception catched : " << e.what() << " where: " << e.info.addr << std::endl;
-    }
-    catch (const std::exception& e)
-    {
+    } catch (const segvcatch::hardware_exception &e) {
+        std::cerr << "Exception catched : " << e.what()
+                  << " where: " << e.info.addr << std::endl;
+    } catch (const std::exception &e) {
         std::cerr << "Exception catched : " << e.what() << std::endl;
     }
 
-    try
-    {
-        #ifndef __arm__ // TODO
+    try {
+#ifndef __arm__ // TODO
         test::divide_by_zero();
-        #endif
-    }
-    catch (const segvcatch::hardware_exception &e)
-    {
-        std::cerr << "Exception catched : " << e.what() << " where: " << e.info.addr << std::endl;
-    }
-    catch (std::exception& e)
-    {
+#endif
+    } catch (const segvcatch::hardware_exception &e) {
+        std::cerr << "Exception catched : " << e.what()
+                  << " where: " << e.info.addr << std::endl;
+    } catch (std::exception &e) {
         std::cerr << "Exception catched : " << e.what() << std::endl;
     }
 
     std::cout << "We are living yet!" << std::endl;
-
-    return 0;
 }
